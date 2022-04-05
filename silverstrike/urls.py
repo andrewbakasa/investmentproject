8ec592ef0c55c6a7951d424221a189506a48ca1d
Andrew Bakasa <bakasa.andrew@gmail.com>
2022-04-05 21:22:03 +0200

    First Commit

--- silverstrike/urls.py
+++ silverstrike/urls.py
@@ -1,9 +1,6 @@
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-from rest_framework import routers
-from rest_framework.authtoken import views as drf_views
-
 urlpatterns = [
   
     path('auth/', include('allauth.urls')),
