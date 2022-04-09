35a5f1e13ada87a1a82d53c396f1340649297b8d
Andrew Bakasa <bakasa.andrew@gmail.com>
2022-04-09 17:06:17 +0200

    fifth commit

--- myproj/staticfiles/mentor/assets/js/function.js
+++
@@ -1,7 +0,0 @@
-const array=[1,2,3,4,1,1,2,34]
-const uniq=array.filter((item,ind) => {
-    return array.indexOf(item) ===index
-
-});
-const removeDups= [... new Set(array)]
-console.log[removeDups]
