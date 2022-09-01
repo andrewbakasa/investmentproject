71c61596e5bb9e65fbb75d3f5af7d3f0d86d0531
Andrew Bakasa <bakasa.andrew@gmail.com>
2022-09-01 14:46:34 +0200

    u

--- myproj/settings_local.py
+++ myproj/settings_local.py
@@ -70,7 +70,11 @@ INSTALLED_APPS = [
       "markers" 
 
 ]
-
+# if not DEBUG:
+#     INSTALLED_APPS.append('django.contrib.gis') 
+#     INSTALLED_APPS.append('leaflet')
+#     INSTALLED_APPS.append('djgeojson') 
+#     INSTALLED_APPS.append("markers") 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -113,7 +117,28 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myproj.wsgi.application'
 
-
+import os
+if os.name == 'nt':
+    import platform
+    OSGEO4W = r"C:\OSGeo4W"
+    
+    if '64' in platform.architecture()[0]:
+        #OSGEO4W += "64"
+        pass
+    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
+# if DEBUG :
+# DATABASES = {
+#     'default': {
+#         #'ENGINE': 'django.db.backends.sqlite3',
+#         'ENGINE':'django.contrib.gis.db.backends.postgis',
+#         # 'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
     'default': {
@@ -124,7 +149,13 @@ DATABASES = {
          'PORT' : '5432',
     },
 }
-
+# else:
+#     import dj_database_url
+#     DATABASES ={}
+#     DATABASES['default'] = dj_database_url.config()
+#     DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+# Password validation
+# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -198,9 +229,20 @@ EMAIL_HOST_USER = config('EM_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EM_HOST_PSWD')
 
 
+#GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
+#GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
+#print(GEOS_LIBRARY_PATH,GDAL_LIBRARY_PATH)
+GDAL_LIBRARY_PATH = r'C:\OSGeo4W\bin\gdal305.dll'
+GEOS_LIBRARY_PATH = 'c:\\Program Files\\PostgreSQL\\14\\bin\\libgeos_c.dll'
 
-GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so' if os.environ.get('ENV') == 'HEROKU' else environ.get('GEOS_LIBRARY_PATH')
-GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so' if os.environ.get('ENV') == 'HEROKU' else environ.get('GDAL_LIBRARY_PATH')
+# print(os.environ.get('ENV'))
+# GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so' if os.environ.get('ENV') == 'HEROKU' else environ.get('GEOS_LIBRARY_PATH')
+# GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so' if os.environ.get('ENV') == 'HEROKU' else environ.get('GDAL_LIBRARY_PATH')
+print(GEOS_LIBRARY_PATH,GDAL_LIBRARY_PATH)
+# if os.name == 'nt':
+#     VENV_BASE = os.environ['VIRTUAL_ENV']
+#     os.environ['PATH'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo') + ';' + os.environ['PATH']
+#     os.environ['PROJ_LIB'] = os.path.join(VENV_BASE, 'Lib\\site-packages\\osgeo\\data\\proj') + ';' + os.environ['PATH']
 
 #uncomment if you want to check all files are collected into staticfile
 STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
